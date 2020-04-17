b4540f2c24ec52c99251713c65f9bba8a359210c
Brittany McDonald <bmcdonald80@my.bcit.ca>
2020-04-17 00:24:52 -0700

    built ingredients, and recipe title comps

--- comps/TutorialCard/index.js
+++ comps/TutorialCard/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 import './tutorialcard.css';
 
-const TutorialStep = ({text, fontSize, color}) => <div 
-style={{backgroundColor:color}}
+const TutorialStep = ({background, text, fontSize, color}) => <div 
+style={{backgroundColor:background}}
 className="tutorial_steps"
 >
-  <h2 className="tutorial_steps_header" style={{}}>Step 1</h2>
+  <h2 className="tutorial_steps_header" style={{color:color}}>Step 1</h2>
 <p className="tutorial_steps_text" style={{fontSize:fontSize}}>{text}</p>
 </div>
 
 TutorialStep.defaultProps = {
   fontSize:16,
-  text:"Select your Country, Meal type, and Portion Size from the menus below."
+  text:"Select your Country, Meal type, and Portion Size from the menus below.",
+  color:"#ffffff",
+  background:"#FF8F3F"
 }
 
 export default TutorialStep;
