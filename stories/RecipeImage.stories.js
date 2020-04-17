58a435f61aca8853f1c2cea8978a02bd4bcb3e55
Brittany McDonald <bmcdonald80@my.bcit.ca>
2020-04-16 20:17:36 -0700

    Built recipe image comp

---
+++ stories/RecipeImage.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RecipeImg from '../comps/RecipeImage';
+
+export default {
+    title:"Thai Curry",
+    component:RecipeImg
+};
+
+export const FoodImage = () => <RecipeImg />;
