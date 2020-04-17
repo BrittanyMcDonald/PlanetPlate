7b796be6768c83bec2dd16f24d6eebb118c1e8c7
Brittany McDonald <bmcdonald80@my.bcit.ca>
2020-04-16 19:58:17 -0700

    built the Header comp

---
+++ stories/Header.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from '../comps/Header';
+
+export default {
+    title:"PlanetPlate",
+    component:Header
+};
+
+export const MyHeader = () => <Header />;
