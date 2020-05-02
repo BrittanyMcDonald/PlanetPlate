8ff770371e20e92bf2e140017856e8226841f40c
Brittany McDonald <bmcdonald80@my.bcit.ca>
2020-05-02 14:31:33 -0700

    organized the remaining files & deleted duplicates

--- dropdown.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import dropdown from '../dropdown/';
-
-export default {
-    component:dropdown
-};
-
-export const dropdown = () => <dropdown />;
--- stories/NavBar.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NavBar from '../comps/NavBar';
-
-export default {
-    title:"Main Menu",
-    component:NavBar
-};
-
-export const MainNavBar = () => <NavBar />;
