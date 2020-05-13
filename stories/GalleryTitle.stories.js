0405e5a9a6ae2c825a5389b9d73ce8843b52802f
aneetromana <55719972+aneetromana@users.noreply.github.com>
2020-05-12 21:40:22 -0700

    Title

---
+++ stories/GalleryTitle.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import GalleryTitle from '/Users/aneetromana/Downloads/PlanetPlate-master 2/comps/GalleryTitle';
+
+export default {
+    title:"GalleryTitle",
+    component:Gallery
+};
+
+export const Gallery = () => <GalleryTitle />;
